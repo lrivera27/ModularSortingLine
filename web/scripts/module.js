33e17656c76acfa7da427da32fd6702de0141411
Luis Rivera <lriveraf.27@gmail.com>
2018-04-02 17:19:09 -0400

    Add module now works, still not finished

--- web/scripts/module.js
+++ web/scripts/module.js
@@ -7,21 +7,17 @@ async function getAllArduinoNames() {
 
   if(arduinoPorts.length > 0) {
     for(let port of arduinoPorts) {
-      arduinos.port = await eel.typeOfPort(port)();
+      let arduino = await eel.typeOfPort(port)();
+      arduino = arduino.split(" ");
+      arduino.shift();
+      arduinos[port] = arduino.join(" ");
     }
+    return arduinos;
   }
 
-  return arduinos;
+  alert("No arduinos found.");
+  return null;
 };
-
-async function createModule() {
-  console.log("Creating Module!");
-};
-
-
-$('#modulePlus').click(function() {
-  createModule();
-});
 
 $('#moduleModal .modal-footer button').on('click', function(event) {
   var $button = $(event.target);
@@ -31,7 +27,204 @@ $('#moduleModal .modal-footer button').on('click', function(event) {
   });
 });
 
-$('#moduleModal').on('click', '#sideModuleButton', function() {
-  alert("Side Module selected!");
-  $('moduleModal').modal('hide');
+
+
+$(document).ready(function() {
+  console.log("Document ready");
+  $('#mainModuleButton').click(async function() {
+    createMainModule();
+  });
+
+  $('#sideModuleButton').click(async function() {
+    createSideModule();
+  });
 });
+
+async function createMainModule() {
+  console.log("Creating Main Module!");
+
+  let arduinos = await getAllArduinoNames();
+
+  if(arduinos) {
+
+    var html ='<br>'+
+    '         <div id="module">'+
+    '          <div class="row" id="moduleTile">'+
+    '            <div class="col-12" id="moduleType">'+
+    '              <h4 class="text-center">Main Module</h4>'+
+    '              <hr>'+
+    '            </div>'+
+    '          </div>'+
+    '          <div class="row">'+
+    '            <div class="col-6" id="moduleIRs">'+
+    '              <div class="row justify-content-md-center">'+
+    '                <div class="col-md-auto">'+
+    '                  <h5 class="text-center component-label component-label">IR 1</h5>'+
+    '                </div>'+
+    '                <div class="col-md-auto">'+
+    '                  <select class="custom-select">'+
+    '                    <option selected>Arduino</option>'+
+    '                    <option value="1">Mega (COM4)</option>'+
+    '                    <option value="2">UNO (COM5)</option>'+
+    '                  </select>'+
+    '                </div>'+
+    '                <div class="col-md-auto">'+
+    '                  <input class="form-control text-center" type="text" placeholder="Arduino Pin">'+
+    '                </div>'+
+    '              </div><br>'+
+    '              <div class="row justify-content-md-center">'+
+    '                <div class="col-md-auto">'+
+    '                  <h5 class="text-center component-label">IR 2</h5>'+
+    '                </div>'+
+    '                <div class="col-md-auto">'+
+    '                  <select class="custom-select">'+
+    '                    <option selected>Arduino</option>'+
+    '                    <option value="1">Mega (COM4)</option>'+
+    '                    <option value="2">UNO (COM5)</option>'+
+    '                  </select>'+
+    '                </div>'+
+    '                <div class="col-md-auto">'+
+    '                  <input class="form-control text-center" type="text" placeholder="Arduino Pin">'+
+    '                </div>'+
+    '              </div>'+
+    '            </div>'+
+    '            <div class="col-6" id="moduleServos">'+
+    '              <div class="row justify-content-md-center">'+
+    '                <div class="col-md-auto">'+
+    '                  <h5 class="text-center component-label">SERVO 1</h5>'+
+    '                </div>'+
+    '                <div class="col-md-auto">'+
+    '                  <select class="custom-select">'+
+    '                    <option selected>Arduino</option>'+
+    '                    <option value="1">Mega (COM4)</option>'+
+    '                    <option value="2">UNO (COM5)</option>'+
+    '                  </select>'+
+    '                </div>'+
+    '                <div class="col-md-auto">'+
+    '                  <input class="form-control text-center" type="text" placeholder="Arduino Pin">'+
+    '                </div>'+
+    '              </div><br>'+
+    '              <div class="row justify-content-md-center">'+
+    '                <div class="col-md-auto">'+
+    '                  <h5 class="text-center component-label">SERVO 2</h5>'+
+    '                </div>'+
+    '                <div class="col-md-auto">'+
+    '                  <select class="custom-select">'+
+    '                    <option selected>Arduino</option>'+
+    '                    <option value="1">Mega (COM4)</option>'+
+    '                    <option value="2">UNO (COM5)</option>'+
+    '                  </select>'+
+    '                </div>'+
+    '                <div class="col-md-auto">'+
+    '                  <input class="form-control text-center" type="text" placeholder="Arduino Pin">'+
+    '                </div>'+
+    '              </div><br>'+
+    '              <div class="row justify-content-md-center">'+
+    '                <div class="col-md-auto">'+
+    '                  <h5 class="text-center component-label">SERVO 3</h5>'+
+    '                </div>'+
+    '                <div class="col-md-auto">'+
+    '                  <select class="custom-select">'+
+    '                    <option selected>Arduino</option>'+
+    '                    <option value="1">Mega (COM4)</option>'+
+    '                    <option value="2">UNO (COM5)</option>'+
+    '                  </select>'+
+    '                </div>'+
+    '                <div class="col-md-auto">'+
+    '                  <input class="form-control text-center" type="text" placeholder="Arduino Pin">'+
+    '                </div>'+
+    '              </div>'+
+    '            </div>'+
+    '          </div>'+
+    '        </div>';
+
+    $('#modules').append(html);
+  }
+};
+
+async function createSideModule() {
+  console.log("Creating Side Module");
+
+  let arduinos = await getAllArduinoNames();
+
+  if(arduinos) {
+
+    var html = '<br>'+
+    '         <div id="module">'+
+    '          <div class="row" id="moduleTile">'+
+    '            <div class="col-12" id="moduleType">'+
+    '              <h4 class="text-center">Regular Module</h4>'+
+    '              <hr>'+
+    '            </div>'+
+    '          </div>'+
+    '          <div class="row">'+
+    '            <div class="col-6" id="moduleIRs">'+
+    '              <div class="row justify-content-md-center">'+
+    '                <div class="col-md-auto">'+
+    '                  <h5 class="text-center component-label">IR 1</h5>'+
+    '                </div>'+
+    '                <div class="col-md-auto">'+
+    '                  <select class="custom-select">'+
+    '                    <option selected>Arduino</option>'+
+    '                    <option value="1">Mega (COM4)</option>'+
+    '                    <option value="2">UNO (COM5)</option>'+
+    '                  </select>'+
+    '                </div>'+
+    '                <div class="col-md-auto">'+
+    '                  <input class="form-control text-center" type="text" placeholder="Arduino Pin">'+
+    '                </div>'+
+    '              </div><br>'+
+    '              <div class="row justify-content-md-center">'+
+    '                <div class="col-md-auto">'+
+    '                  <h5 class="text-center component-label">IR 2</h5>'+
+    '                </div>'+
+    '                <div class="col-md-auto">'+
+    '                  <select class="custom-select">'+
+    '                    <option selected>Arduino</option>'+
+    '                    <option value="1">Mega (COM4)</option>'+
+    '                    <option value="2">UNO (COM5)</option>'+
+    '                  </select>'+
+    '                </div>'+
+    '                <div class="col-md-auto">'+
+    '                  <input class="form-control text-center" type="text" placeholder="Arduino Pin">'+
+    '                </div>'+
+    '              </div>'+
+    '            </div>'+
+    '            <div class="col-6" id="moduleServos">'+
+    '              <div class="row justify-content-md-center">'+
+    '                <div class="col-md-auto">'+
+    '                  <h5 class="text-center component-label">SERVO 1</h5>'+
+    '                </div>'+
+    '                <div class="col-md-auto">'+
+    '                  <select class="custom-select">'+
+    '                    <option selected>Arduino</option>'+
+    '                    <option value="1">Mega (COM4)</option>'+
+    '                    <option value="2">UNO (COM5)</option>'+
+    '                  </select>'+
+    '                </div>'+
+    '                <div class="col-md-auto">'+
+    '                  <input class="form-control text-center" type="text" placeholder="Arduino Pin">'+
+    '                </div>'+
+    '              </div><br>'+
+    '              <div class="row justify-content-md-center">'+
+    '                <div class="col-md-auto">'+
+    '                  <h5 class="text-center component-label">SERVO 2</h5>'+
+    '                </div>'+
+    '                <div class="col-md-auto">'+
+    '                  <select class="custom-select">'+
+    '                    <option selected>Arduino</option>'+
+    '                    <option value="1">Mega (COM4)</option>'+
+    '                    <option value="2">UNO (COM5)</option>'+
+    '                  </select>'+
+    '                </div>'+
+    '                <div class="col-md-auto">'+
+    '                  <input class="form-control text-center" type="text" placeholder="Arduino Pin">'+
+    '                </div>'+
+    '              </div>'+
+    '            </div>'+
+    '          </div>'+
+    '        </div>';
+
+    $('#modules').append(html);
+  }
+}
